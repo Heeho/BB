418dc115c9f8f35668a0e70a0d18575cb9d852fe
Heeho <feenixbox@gmail.com>
2023-10-15 18:11:45 +0300

    Update State.kt
    WIP States

--- core/src/ru/ltow/bb/component/State.kt
+++ core/src/ru/ltow/bb/component/State.kt
@@ -1,8 +1,38 @@
 package ru.ltow.bb.component
 
 import com.badlogic.ashley.core.Component
-import ru.ltow.bb.system.StateMachine
 
-class State: Component {
-    var current = StateMachine.State.STAND
+class State(val owner: Entity): Component {
+  enum class Value { STAND,WALK,FALL }
+  private val current = setOf(Value.STAND)
+  //сортируем для анимации
+  { s1,s2 =>
+    s1.ordinal < s2.ordinal
+  }
+
+  private val flags =
+    HashMap<Value,Boolean>
+
+  init {
+    Value.values.forEach { flags.add(it,false) }
+  }
+
+  fun setflag(
+    key: Value,
+    value: Boolean
+  ) {
+    if flags.put(key,value) == value
+      return
+    stateChanged.dispatch(owner)
+  }
+
+  fun getflags(): Map = flags
+
+  fun getanimation(): Value = current.first
+
+  fun getstates(): List = current.values
+
+  companion object {
+    val stateChanged = Signal<Entity>()
+  }
 }
