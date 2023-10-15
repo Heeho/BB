3ed5fb0cf0bef571d0c105ad230f38919b273110
Heeho <feenixbox@gmail.com>
2023-10-15 18:17:21 +0300

    Update Controller.kt
    WIP States

--- core/src/ru/ltow/bb/system/Controller.kt
+++ core/src/ru/ltow/bb/system/Controller.kt
@@ -4,15 +4,16 @@ import com.badlogic.ashley.core.*
 import com.badlogic.ashley.utils.ImmutableArray
 import ru.ltow.bb.component.State
 
-class Controller: EntitySystem() {
-    private lateinit var entities: ImmutableArray<Entity>
-    private lateinit var stateMapper: ComponentMapper<State>
+class Controller(
+  val onDrag: () => Unit
+    //e.motion.vector.set(dragv)
+): EntitySystem {
+  //val e = (player)
 
-    override fun addedToEngine(engine: Engine?) {
-        if(engine != null) {
-            entities = engine.getEntitiesFor(Family.all(State::class.java).get())
-            stateMapper = ComponentMapper.getFor(State::class.java)
-        }
-        super.addedToEngine(engine)
-    }
+  fun onDragStart() {
+    //e.state.setflag(State.Value.WALK,true)
+  }
+  fun onDragStop() {
+    //e.state.setflag(State.Value.WALK,false)
+  }
 }
