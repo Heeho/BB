dbfb9874d0efb57bc9f65304ab6fc2b3f82eff33
Heeho <feenixbox@gmail.com>
2023-11-12 20:22:35 +0300

    WIP State

--- core/src/ru/ltow/bb/system/StateMachine.kt
+++ core/src/ru/ltow/bb/system/StateMachine.kt
@@ -3,24 +3,22 @@ package ru.ltow.bb.system
 import com.badlogic.ashley.core.*
 import com.badlogic.gdx.math.Vector2
 
-class StateMachine(
-  stateChanged: Listener
-): EntitySystem {
- override fun update(delta: Float) {
-  stateChanged.forEach {
-   val flags it.state.getflags()
-    it.state.current.(add,remove) = f(flags)
-    state.getstates().forEach {
-     it.run()
+class StateMachine: EntitySystem {
+  private lateinit var stateEntities: ImmutableArray<Entity>
+  private lateinit var stateMapper: ComponentMapper<State>
+
+  override fun addedToEngine(engine: Engine?) {
+    if(engine != null) {
+      stateEntities = engine.getEntitiesFor(Family.one(State::class.java).get())
+      stateMapper = ComponentMapper.getFor(State::class.java)
     }
-   }
+    super.addedToEngine(engine)
   }
-    companion object {
-        fun calculateFace(v: Vector2): Face {
-            return if (v.x >= 0 && v.y >= 0) Face.NE
-            else if (v.x >= 0 && v.y < 0) Face.SE
-            else if (v.x < 0 && v.y >= 0) Face.NW
-            else Face.SW
-        }
+
+  override fun update(delta: Float) {
+    entities.forEach { e ->
+      stateMapper.get(e).actions.forEach { a ->
+        a.act(s)
     }
+  }
 }
