67694a2c9bf2fc2ec7586e5238cbef37a4013f28
Heeho <feenixbox@gmail.com>
2024-01-03 12:46:14 +0300

    Face.kt
    WIP Animations

---
+++ core/src/ru/ltow/bb/component/Face.kt
@@ -0,0 +1,8 @@
+package ru.ltow.bb.component
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.math.Vector3
+
+class Face(
+  var vector: Vector3 = Vector3(1f,0f,0f)
+): Component
