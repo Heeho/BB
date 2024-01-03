0e2241b59a56a430bc4e3163dfdc7f2557bae87a
Heeho <feenixbox@gmail.com>
2024-01-03 20:54:36 +0300

    Update Controller.kt
    WIP Controls

--- core/src/ru/ltow/bb/system/Controller.kt
+++ core/src/ru/ltow/bb/system/Controller.kt
@@ -4,37 +4,30 @@ import com.badlogic.ashley.core.*
 import com.badlogic.ashley.utils.ImmutableArray
 import com.badlogic.gdx.Gdx
 import ru.ltow.bb.component.Player
-import ru.ltow.bb.component.State
 
 class Controller: EntitySystem() {
-  private lateinit var playerEntities: ImmutableArray<Entity>
-  private lateinit var stateMapper: ComponentMapper<State>
+  val standers: ImmutableArray<Entity>
+  val walkers: ImmutableArray<Entity>
 
-  override fun addedToEngine(engine: Engine?) {
-    if(engine != null) {
-      playerEntities = engine.getEntitiesFor(Family.one(Player::class.java).get())
-      stateMapper = ComponentMapper.getFor(State::class.java)
+  val DRAG_SENSIVITY = 10f
+
+  override fun addedToEngine(e: Engine) {
+    standers = e.getEntitiesFor(Family.all(Player::class.java,Stand::class.java).get())
+    walkers = e.getEntitiesFor(Family.all(Player::class.java,Walk::class.java).get())
+    super()
+  }
+
+  override fun update(dt: Float) {
+    standers.forEach() { e ->
+      if(GDX.input.touchDragged() && drag.length > DRAG_SENSIVITY)
+        e.add(Walk())
     }
-    super.addedToEngine(engine)
-  }
 
-  override fun update(deltaTime: Float) {
-    /*
-    Gdx.*
-    touchDown
-    touchDragged
-    touchUp
-     */
-  }
-
-  fun onDragStart() {
-    playerEntities.forEach {
-      stateMapper.get(it).start(State.Action.WALK)
-    }
-  }
-  fun onDragStop() {
-    playerEntities.forEach {
-      stateMapper.get(it).end(State.Action.WALK)
+    walkers.forEach() { e ->
+      if(GDX.input.touchUp())
+        e.add(Stand())
+      else
+        Mappers.walk.get(e).vector = drag.vector
     }
   }
 }
